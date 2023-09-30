e4b49a2db366ea1b90a49bbdb01dfb5d11619473
tom <nqwrt@naver.com>
2023-10-01 00:43:41 +0900

    first commit. 기본설정

---
+++ src/main/java/edu/human/ex/SpringBootIntellijGradleJpaApplication.java
@@ -0,0 +1,13 @@
+package edu.human.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntellijGradleJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntellijGradleJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/human/ex/SpringBootIntellijGradleJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.human.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntellijGradleJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
